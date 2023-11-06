7b04721edf53027f23e0630220d16783de052483
Saikat Dey <30950554+deysaikat04@users.noreply.github.com>
2023-11-06 08:58:27 +0530

    feat: zod validation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // import "./App.css";
 import { SimpleRegistrationForm } from "./components/simple/SimpleRegistrationForm";
 import { YupRegistrationForm } from "./components/yup/YupRegistrationForm";
+import { ZodRegistrationForm } from "./components/zod/ZodRegistrationForm";
 
 function App() {
   return (
     <div className="flex flex-col">
       <SimpleRegistrationForm />
       <YupRegistrationForm />
+      <ZodRegistrationForm />
     </div>
   );
 }
